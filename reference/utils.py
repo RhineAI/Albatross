1877b01da57f913caa382ef7843c390b3bfd57a2
Bo <a@a.com>
2025-09-19 12:56:27 +0800

    faster

--- reference/utils.py
+++ reference/utils.py
@@ -40,12 +40,15 @@ def sample_logits(logits, temperature:float=1.0, top_p:float=1.0, top_k:int=0):
 
     return torch.multinomial(probs, num_samples=1).item()
 
-def sampler_simple_batch(logits, noise:float=1.0, temp:float=1.0):
+@MyStatic # !!! will modify logits inplace !!!
+def sampler_simple_batch(logits: torch.Tensor, noise: float = 1.0, temp: float = 1.0):
     assert temp > 0, "use noise=0 for greedy decoding"
-    if temp != 1.0:
-        logits = logits * (1.0 / temp)
-    logits = logits + torch.zeros_like(logits).uniform_(0, noise)
-    return torch.argsort(logits, dim=-1, descending=True)[:, :1]
+    with torch.no_grad():
+        if temp != 1.0:
+            logits.mul_(1.0 / temp)
+        if noise != 0.0:
+            logits.add_(torch.empty_like(logits).uniform_(0.0, noise))
+        return torch.argmax(logits, dim=-1, keepdim=True)
 
 class TRIE:
     __slots__ = tuple("ch,to,values,front".split(","))
